17687cb7c7a75a71c63eeded0174929c9922e38e
tibebesaas <louabi@hotmail.com>
2025-07-09 09:13:17 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -113,3 +113,24 @@ function toggleMenu() {
     }, 400);
   }
 });
+document.addEventListener("DOMContentLoaded", () => {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.toggle("active", i === index);
+    });
+  }
+
+  function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  if (slides.length > 0) {
+    showSlide(currentSlide);
+    setInterval(nextSlide, 3000);
+  }
+});
+
