c979b30efe51569158688be12d57b411e0d866f6
tibebesaas <louabi@hotmail.com>
2025-07-09 09:59:15 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -116,24 +116,3 @@ function toggleMenu() {
     }, 400);
   }
 });
-document.addEventListener("DOMContentLoaded", () => {
-  let currentSlide = 0;
-  const slides = document.querySelectorAll(".slide");
-
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      slide.classList.toggle("active", i === index);
-    });
-  }
-
-  function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length;
-    showSlide(currentSlide);
-  }
-
-  if (slides.length > 0) {
-    showSlide(currentSlide);
-    setInterval(nextSlide, 3000);
-  }
-});
-
